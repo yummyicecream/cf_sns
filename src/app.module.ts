010d948451188dcb2f39ec388a1b3007440885bb
joonho <minhora1@gmail.com>
2024-01-26 00:37:41 +0900

    feat: user module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
