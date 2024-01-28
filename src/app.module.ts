93240090c5f7e7a9327df01ebd11ebad2245750d
joonho <minhora1@gmail.com>
2024-01-28 22:20:07 +0900

    feat: comments root 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [PostsModule, UserModule],
+  imports: [PostsModule, UserModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
