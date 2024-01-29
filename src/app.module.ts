ceba58f0a8a4a7fc0b8340faa0672525504ea7e4
joonho <minhora1@gmail.com>
2024-01-29 23:55:15 +0900

    feat: chat rood 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
 import { CommentsModule } from './comments/comments.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PostsModule, UserModule, CommentsModule],
+  imports: [PostsModule, UserModule, CommentsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
